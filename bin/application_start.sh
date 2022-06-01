17722e5a12076afd63fca2f2988f1a724a815cac
Ryan Jung <hgjung@hansung.ac.kr>
2022-06-01 12:59:25 +0900

    Add files via upload

---
+++ bin/application_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Stop all servers and start the server as a daemon
+# forever stopall
+sudo npm install -g node-gyp
+npm install bcrypt
+
+forever start /home/ec2-user/node-js/server.js
