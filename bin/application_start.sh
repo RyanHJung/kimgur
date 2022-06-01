490270f41fb707cdad4d180af8a2830585c30068
Ryan Jung <hgjung@hansung.ac.kr>
2022-06-01 14:13:30 +0900

    Update application_start.sh

--- bin/application_start.sh
+++ bin/application_start.sh
@@ -2,13 +2,7 @@
 
 # Stop all servers and start the server as a daemon
 # forever stopall
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
 sudo npm install -g node-gyp
 npm install bcrypt
-cd node-js
-npm init
-npm start
 
 # forever start /home/ec2-user/node-js/server.js
